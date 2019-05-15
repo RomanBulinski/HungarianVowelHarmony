30d7d1f70aeca8d85f9f9eb80f361efa26204b8f
Roman Bulinski <aboandrus@gmail.com>
2019-05-15 18:06:40 +0200

    Refactor code ( used contains function )

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,5 @@
 public class Main {
 
-//    Kata.dative("ablak") == "ablaknak"
-//            Kata.dative("szék") == "széknek"
-//            Kata.dative("otthon") == "otthonnak"
-
     public static String dative(String word) {
 
         int len = word.length();
@@ -15,7 +11,7 @@ public class Main {
             char let4 = letters[letters.length - 4];
             String x = getNewWord(let2, let3, word);
             if (x != null) return x;
-            if (  let3 == 's' && let4 == 'o') {
+            if ( let4 == 'o') {
                 return word + "nak";
             }
         }else if ( len <= 3 ){
@@ -25,31 +21,23 @@ public class Main {
         return word;
     }
 
-//    expected:<kérvény[nek]> but was:<kérvény[]>
 
+    private static String getNewWord( char let2, char let3, String word) {
 
-    private static String getNewWord(char let2, char let3, String word) {
-        if (let2 == 'a' || let2 == 'á' || let2 == 'o' || let2 == 'ó' || let2 == 'u' || let2 == 'ú') {
+        if ( "aáoóuú".contains( String.valueOf(let2) ) ){
             return word + "nak";
         }
-        if (let2 == 'e' || let2 == 'é' || let2 == 'i' || let2 == 'ö' || let2 == 'ő' || let2 == 'ü' || let2 == 'ű') {
+        if ( "eéiöőüű".contains( String.valueOf(let2)) ) {
             return word + "nek";
         }
         if ( let3 == 'e' || let3 == 'é' ) {
             return word + "nek";
         }
-        if ( let3 == 'a') {
+        if ( let3 == 'a' || let3 == 'o' || let3 == 'á' ) {
             return word + "nak";
         }
-
-        if (let2 == 's' && let3 == 'o') {
-            return word + "nak";
-        }
-        if (let3 == 'á' && let2 == 'l') {
-            return word + "nak";
-        }
-
         return null;
     }
+
 
 }
